e68b18e8b3b90fe915de143a0c729cb6ddb91af7
edponce <edponce2010@gmail.com>
2018-01-12 03:17:22 -0500

    added PaPaS configuration file for extensions

--- papas.py
+++ papas.py
@@ -16,16 +16,25 @@ def parseArgs():
              description='JSON Parser: parse JSON configuration file',
              formatter_class=RawTextHelpFormatter)
 
-    parser.add_argument('-j', '--jsonfile', type=str, dest='jsonfile',
+    parser.add_argument('-c', '--conf', type=str, dest='papas_conf_file',
+                        default='papas_conf.json',
+                        help='PaPaS JSON configuration file\n'
+                             'Default is papas_conf.json')
+
+    parser.add_argument('-a', '--app', type=str, dest='app_conf_file',
                         default='',
-                        help='JSON configuration file')
+                        help='Application JSON configuration file')
 
     args = parser.parse_args()
 
     # Validate options
     err = 0
-    if not os.path.isfile(args.jsonfile):
-        print('ERROR: JSON configuration file does not exist, ' + args.jsonfile)
+    if not os.path.isfile(args.app_conf_file):
+        print('ERROR: Application JSON configuration file does not exist, ' + args.app_conf_file)
+        err += 1
+
+    if not os.path.isfile(args.papas_conf_file):
+        print('ERROR: PaPaS JSON configuration file does not exist, ' + args.papas_conf_file)
         err += 1
 
     if err != 0:
@@ -36,29 +45,37 @@ def parseArgs():
     return args
 
 
-def processJSONfile(jsonfile=''):
+def loadConfData(conf_file=''):
     '''
-    Load and parse JSON configuration file
+    Load a JSON configuration file
     '''
-    jsonfile = os.path.abspath(jsonfile)
+    conf_data = {}
+    with open(os.path.abspath(conf_file), 'r') as f:
+        conf_data = json.load(f)
 
-    # Load JSON data
-    # Mandatory keywords are: program#, params#, and dependences
-    with open(jsonfile, 'r') as f:
-        config = json.load(f)
+    return conf_data
+
+
+def processAppConf(papas_conf_data={}, app_conf_data={}):
+    '''
+    Parse configuration data
+    Application mandatory keys are: program#, params#, and dependences
+    PaPaS mandatory keys are: extensions
+    '''
 
     # Construct command line
     cmd = []
 
+    # Check for env variables pre-application.
     # Check for extensions to auto-detect C/C++/Python/Java/etc.
     # Append ./ only if single word and it is an executable
-    prog_name = config['program1']
+    prog_name = app_conf_data['program1']
     if os.access(prog_name, os.X_OK):
         cmd.append('./' + prog_name)
     else:
         cmd.append(prog_name)
 
-    for param, vals in config['params1'].items():
+    for param, vals in app_conf_data['params1'].items():
         if isinstance(vals, list) and len(vals) > 1:
             for val in vals:
                 cmd.append(param)
@@ -76,5 +93,7 @@ Main entry point
 '''
 if __name__ == "__main__":
     args = parseArgs()
-    processJSONfile(args.jsonfile)
+    papas_conf_data = loadConfData(args.papas_conf_file)
+    app_conf_data = loadConfData(args.app_conf_file)
+    processAppConf(papas_conf_data, app_conf_data)
 
